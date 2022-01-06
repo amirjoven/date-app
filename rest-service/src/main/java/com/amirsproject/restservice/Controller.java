1c4f2e046cd73a337eea96d2eb366b5065948490
Amir Franco Jove <amirjoven@deltek.com>
2022-01-06 18:00:01 +0800

    Added rest-service app

---
+++ rest-service/src/main/java/com/amirsproject/restservice/Controller.java
@@ -0,0 +1,21 @@
+package com.amirsproject.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class Controller {
+ 
+    @GetMapping("/getserverdate")
+    public ServerDateResource getserverdate() {
+		return  new ServerDateResource();
+	}
+
+    @GetMapping("/")
+    public ServerDateResource root() {
+		return new ServerDateResource();
+	}    
+    
+}
---
+++ rest-service/src/main/java/com/amirsproject/restservice/ServerDateResource.java
@@ -0,0 +1,32 @@
+package com.amirsproject.restservice;
+
+import java.time.ZoneId;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+
+public class ServerDateResource {
+    ZoneId z = ZoneId.systemDefault();
+    private Date serverDate = null;
+
+    public ServerDateResource(){
+        serverDate = new Date();
+    }
+
+    public String getServerDate(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");          
+        return formatter.format(serverDate);
+    }
+	
+    public String getServerTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");              
+        return formatter.format(serverDate);
+    }    
+	
+    public String getServerTimezone(){
+        return z.toString();        
+    }
+	
+	public long getServerUnixTimestamp(){
+		return serverDate.getTime() / 1000L;
+	}
+}
